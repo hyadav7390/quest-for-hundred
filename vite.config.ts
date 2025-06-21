e398c4a5d1e6da2926a81edff3413c544126ab1e
Himanshu Yadav <Hyadav7390@gmail.com>
2025-06-22 00:13:04 +0530

    buffer error fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -17,6 +18,18 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      // point all `import 'buffer'` to the shim
+      buffer: 'buffer/'
     },
   },
+  optimizeDeps: {
+    esbuildOptions: {
+      // make sure `global` becomes `globalThis`
+      define: { global: 'globalThis' },
+      plugins: [
+        // polyfill `process`, `Buffer`, etc.
+        NodeGlobalsPolyfillPlugin({ buffer: true })
+      ]
+    }
+  }
 }));
