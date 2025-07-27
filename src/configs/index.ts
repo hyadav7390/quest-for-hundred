b3bd14c2e3db17b68617da78e41845b48e26bc0a
Apple <apple@Apples-MacBook-Pro.local>
2025-07-27 19:22:33 +0530

    latest game contract

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -31,6 +31,6 @@ export const REWARD_TOKEN = {
 export const SINGLE_PLAYER_CONTRACT_ADDRESS = '0x22ea5b45f8209c2fd7e9baba1ee2559194f48e12';
 
 // TODO: Replace with actual deployed multiplayer contract address
-export const MULTI_PLAYER_CONTRACT_ADDRESS = '0x79b5501f36d85f0bc32d6825536a9b067dc412b3';
+export const MULTI_PLAYER_CONTRACT_ADDRESS = '0x3ab3160fdd12e172c7332acdfd97683249921321';
 
 export type TokenSymbol = typeof NATIVE_TOKEN.symbol | typeof REWARD_TOKEN.symbol;
