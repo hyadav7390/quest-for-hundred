05039c044464b06ffcf0a41a0a6c90f458b69243
Apple <apple@Apples-MacBook-Pro.local>
2025-08-08 01:37:38 +0530

    latest game contract

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -25,12 +25,12 @@ export const REWARD_TOKEN = {
   name: 'Rugg Roll Token',
   symbol: 'ROLL',
   decimals: 18,
-  address: '0xdae511d02ae7c9017edd634142f7a0e8e24c8fcd',
+  address: '0xf307322cd2e5e9889ad7933fe2a909e2b95dd5d3',
 };
 
-export const SINGLE_PLAYER_CONTRACT_ADDRESS = '0x0b41d5d0c5a198aa4bf58fa6497d0c019eb7d8be';
+export const SINGLE_PLAYER_CONTRACT_ADDRESS = '0xc99d63935cbf0c36adb320bf860310225a4689b0';
 
 // TODO: Replace with actual deployed multiplayer contract address
-export const MULTI_PLAYER_CONTRACT_ADDRESS = '0x1695111ae37ca6691ebb85862979214733c44bd0';
+export const MULTI_PLAYER_CONTRACT_ADDRESS = '0xeae35f59e802fbfdb73965bf9fee57f0f2049569';
 
 export type TokenSymbol = typeof NATIVE_TOKEN.symbol | typeof REWARD_TOKEN.symbol;
